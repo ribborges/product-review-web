d528c2d585662117e048ce7b3cf44c3edec24fc3
Richard Borges <ribborges@outlook.com>
2025-05-28 01:17:44 -0300

    feat: implement routing with BrowserRouter

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,7 +1,24 @@
+import { Outlet } from "react-router";
+import { HouseFill } from "react-bootstrap-icons";
+
+import { Header } from "@/components/Header";
+import { Navbar, NavItem } from "@/components/Navbar";
+
 export default function App() {
   return (
-    <>
-      A
-    </>
+    <div className="flex flex-col h-screen w-screen overflow-hidden">
+      <Header />
+      <div className="flex-1 flex md:flex-row flex-col-reverse overflow-hidden">
+        <Navbar>
+          <NavItem to="/">
+            <HouseFill size={24} />
+            <span>Início</span>
+          </NavItem>
+        </Navbar>
+        <main className="flex-1 flex md:m-1 bg-zinc-100 dark:bg-zinc-950 md:rounded-xl overflow-hidden">
+          <Outlet />
+        </main>
+      </div>
+    </div>
   );
 }
--- src/app/main.tsx
+++ src/app/main.tsx
@@ -1,12 +1,22 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from 'react-router';
 
 import App from './App.tsx';
+import HomePage from './pages/Home.tsx';
+import NotFoundPage from './pages/NotFound.tsx';
 
-import './styles/global.css';
+import '@/styles/global.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />}>
+          <Route path="/" element={<HomePage />} />
+          <Route path="*" element={<NotFoundPage />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   </StrictMode>
 );
